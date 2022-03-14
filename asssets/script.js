a08f6d5a319eb65da734c5ea75134bd4d0982338
Youssef <youssefkaroui6@gmail.comm>
2022-03-14 13:15:29 -0400

    still

--- asssets/script.js
+++ asssets/script.js
@@ -1,6 +1,65 @@
 // Assignment code here
 
 
+function getLength() {
+  // get length as an input from the user; if input is somehow inocrrect (not a number or not the write number), ask again
+  var promptLength = window.prompt("What would you like the length of the password to be")
+  promptLength = parseInt(promptLength)
+
+  if (isNaN(promptLength)) {
+    window.alert("please enter a numeric value")
+    return getLength();
+  } 
+
+  if (promptLength < 8 || promptLength > 128) {
+    window.alert("please enter at least 8 and no more than 128")
+    return getLength()
+  }
+
+  return promptLength;
+  
+}
+
+function getCharacters() {
+  // get characters desired from user; if user selects no characters, tell them they must, ask again
+  var possibleCharacters = {
+    lowers: "abcdefghijklmnopqrstuvwxyz",
+    uppers: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    symbols: "!@#$%^&*()_+?~"
+  }
+  var promptCharacters = "";
+  var wantsLowers = window.confirm("do you want lower case characters?")
+  if (wantsLowers) {
+    promptCharacters+=possibleCharacters.lowers;
+  }
+  var wantsUppers =window.confirm("do you want upper case characters?")
+  if (wantsUppers) {
+    promptCharacters+=possibleCharacters.uppers;
+  }
+  var wantsNumbers= window.confirm ("do you want numeric characters?")
+  if (wantsNumbers) {
+    promptCharacters+=possibleCharacters.numbers;
+  }
+  var wantsSymbols= window.confirm("do you want symbol characters?")
+  if (wantsSymbols) {
+    promptCharacters+= possibleCharacters.symbols;
+  }
+  return (promptCharacters);
+  // return a string
+
+}
+
+function generatePassword() {
+  var length = getLength() // get us the length of the password
+  console.log(length)
+  var characters = getCharacters();
+  var generateString(length) {
+    
+  }
+
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -12,6 +71,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
