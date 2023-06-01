6394547400bd931d44e182b50270d828cdef14a8
Youssef <youssefkaroui6@gmail.comm>
2023-06-01 13:26:43 -0400

    get length function is reday

--- develop/script.js
+++ develop/script.js
@@ -1,5 +1,25 @@
 // Assignment code here
 
+// a function to get the lenght of the password as an input from the user in the for of an interger
+function getLength ()  {
+  var promptLength= window.prompt("What would you like the length of the password to be?")
+  promptLength = parseInt (promptLength)
+  if (isNaN(promptLength)){
+    window.alert("please enter a numeric value")
+    return getLength();
+
+  }
+
+  if (promptLength < 8 || promptLength > 128) {
+    window.alert("please enter a number between 8 and 128")
+    return getLength()
+    
+
+  }
+  return promptLength;
+
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
